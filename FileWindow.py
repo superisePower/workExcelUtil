955ee7b2cd5a55bdbd810f40cb9fae4b288c3d75
012433930 <375283581@qq.com>
2019-11-22 15:40:53 +0800

    2.2.3.发明名称与原始名称简单重复 说明：仅需审核名称长度小于等于18的，其他无需审核，直接删除。

--- FileWindow.py
+++ FileWindow.py
@@ -6,7 +6,7 @@ from numpy import array
 
 root = tk.Tk(className="工作单")
 root.geometry('600x400')
-textvar = tk.StringVar()  # 如果我们要动态修改Label标签的显示文本，我们可以创建一个变量容器，将这个容器与我们的控件属性绑定即可
+
 
 # 源文件
 def get_file_name():
@@ -22,18 +22,19 @@ def save_file():
 
 # 执行excel操作
 def run_excel():
-    textvar = "begin"
-    path = t.get();
+    label["text"] = "数据处理中......"
+    path = t.get()
     # path = "E:\\workSheet.xls"
     savePath = t2.get()
     # savePath = "E:\\workSheet11.xls"
     wb = pd.read_excel(path, sheet_name='Sheet1')  # 读取excel
     wb = delete_by_word(wb)
+    wb = delete_by_size(wb)
     wb.to_excel(savePath, sheet_name='Sheet1', index=False, header=True)
-    textvar = "success"
+    label["text"] = "success"
 
 
-
+# 根据关键词删除
 def delete_by_word(wb):
     data = wb.iloc[:, 1].values  # 读取需要做筛选的列的数据
     # 查找有此文本“附图标记全部缺失”的行
@@ -73,6 +74,24 @@ def delete_by_word(wb):
 
 
 '''
+    发明名称与原始名称简单重复
+    仅需审核名称长度小于等于18的，其他无需审核，直接删除。
+    '''
+def delete_by_size(wb):
+    data = wb.loc[:, '错误类型'].values  # 读取需要做筛选的列的数据
+    # data = wb.iloc[:, 1].values  # 读取需要做筛选的列的数据
+    data = wb[(data == '发明名称与原始名称简单重复')]  # 筛选出需要的数据
+    arrWord = data.get("备注2")  # 需要筛选的数据列
+    arrIndex = array(data.index)  # 数据列对应的excel index
+
+    for index, value in enumerate(arrWord):
+        value = value.replace("名称长度为：", "")
+        if int(value)>=19:
+            wb.drop(arrIndex[index], axis=0, inplace=True)  # 按照excel的索引删除
+    return wb
+
+
+'''
 定义界面
 '''
 b = tk.Button(root, text='数据源文件', width=10, height=2, command=get_file_name).pack()
@@ -82,15 +101,11 @@ b2 = tk.Button(root, text='另存为', width=10, height=2, command=save_file).pa
 t2 = tk.Entry(borderwidth=3, width=50)
 t2.pack()
 submitButton = tk.Button(root, text='开始', width=10, height=2, command=run_excel).pack()
-label = tk.Label(root, textvariable=textvar)  # text为显示的文本内容
+label = tk.Label(root)  # text为显示的文本内容
 label.pack()
-
-
-
 
 root.mainloop()
 
-
-
+# 用来单元测试
 # if __name__ == "__main__":
 #     run_excel()
